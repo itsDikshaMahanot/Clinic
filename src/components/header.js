209e43ec0eb555db927f07e58886a2f13cd4c75b
Diksha Mahanot <59890161+itsDikshaMahanot@users.noreply.github.com>
2020-09-27 18:54:09 +0530

    Header ready.

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <header>
+        hello
+    </header>
+   )
+
+ }
+
+export default Header
