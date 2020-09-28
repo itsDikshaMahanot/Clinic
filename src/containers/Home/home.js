509792af02c01955cb970a7db34645eee0f4a3e6
Diksha Mahanot <59890161+itsDikshaMahanot@users.noreply.github.com>
2020-09-28 17:06:39 +0530

    images need to enter again

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './home.css';
+import { Row, Col } from 'react-bootstrap';
+
 
 /**
 * @author
@@ -8,9 +10,12 @@ import './home.css';
 
 const Home = (props) => {
   return(
-    <div>
-          <h1>Home</h1>
-          <img src="public/Images/massage.jpg"></img>
+    <div className = "back">
+      <Row>
+      <Col md={2} sm={5}> <img className="profile" src="../Images/pic1.jpg" alt ="Profile"></img></Col>
+      <Col md={10} sm={7}> <h4 className="">This Dr Sonal Chopra .<br/> The Awesome NAutropathy <i class="fa fa-align-center" aria-hidden="true"></i></h4></Col>
+      </Row>
+         
    </div>
    )
 
